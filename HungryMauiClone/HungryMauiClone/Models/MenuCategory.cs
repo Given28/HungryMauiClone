ffdc8dd491f294443fef595f8eb7ad073d0db901
Given28 <168072758+Given28@users.noreply.github.com>
2025-02-05 15:16:12 +0200

    commit

--- HungryMauiClone/HungryMauiClone/Models/MenuCategory.cs
+++ HungryMauiClone/HungryMauiClone/Models/MenuCategory.cs
@@ -10,5 +10,6 @@ namespace HungryMauiClone.Models
     {
         public string Name { get; set; }
         public string Image { get; set; }
+        public double Price { get; set; } // Added Price property
     }
 }
--- HungryMauiClone/HungryMauiClone/ViewModels/MenuViewModel.cs
+++ HungryMauiClone/HungryMauiClone/ViewModels/MenuViewModel.cs
@@ -10,17 +10,18 @@ namespace HungryMaui.ViewModels;
 
 public class MenuViewModel
 {
+
     public ObservableCollection<MenuCategory> Categories { get; set; }
 
     public MenuViewModel()
     {
         Categories = new ObservableCollection<MenuCategory>
         {
-            new MenuCategory { Name = "Big Bacon Burger", Image = "Big_Bacon_Burger.jpg" },
-            new MenuCategory { Name = "Whopper", Image = "whopper.png" },
-            new MenuCategory { Name = "Ribs and Chips", Image = "Ribs_and_Chips.jpg" },
-            new MenuCategory { Name = "Full Chicken", Image = "Full_Chicken.jpg" },
-            new MenuCategory { Name = "Beef Burger", Image = "Beef_Burger.jpg" }
+            new MenuCategory { Name = "Big Bacon Burger", Image = "Big_Bacon_Burger.jpg", Price = 49.99 },
+            new MenuCategory { Name = "Whopper", Image = "whopper.png", Price = 69.49 },
+            new MenuCategory { Name = "Ribs and Chips", Image = "Ribs_and_Chips.jpg", Price = 59.99 },
+            new MenuCategory { Name = "Full Chicken", Image = "Full_Chicken.jpg", Price = 149.99 },
+            new MenuCategory { Name = "Beef Burger", Image = "Beef_Burger.jpg", Price = 89.99 }
         };
     }
 }
